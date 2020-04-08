6a78165ed0ec49575eaf8b0295c6795112e7b605
Erica T Jarvis Mason <etmason@ucsd.edu>
2020-04-08 16:42:22 -0700

    Add files via upload

---
+++ LogReg_Supp.R
@@ -0,0 +1,104 @@
+# Multinomial logistic regression
+# test for opinion change of CCFRP volunteers on MPAs
+# across different levels of volunteer participation
+
+# Three measures of volunteer participation:
+# 1) Years since joining CCFRP,
+# 2) Number of sampling trips attended,
+# 3) Number of Volunteer Appreciation (Data) Workshop events.
+
+library(haven) # read spss files
+library(tidyverse) # dplyr and ggplot
+library(nnet) # multinom function
+library(broom) # test individual effects
+library(car) # Anova table on multinom object
+library(MNLpred) # predicted values
+
+df <- read_sav("survdata.sav")
+names(df)
+
+mydata <- df %>% # select and compute variables of interest
+  dplyr::select("Q1","Q2","Q3","Q6A", "Q11", "Q14_1","Q18_1") %>% 
+  rename(YrStart = "Q1", YrsVol = "Q2", Tpy = "Q3", Evnts = "Q6A",
+         Before = "Q14_1", After = "Q18_1") %>% 
+         drop_na(Before) %>% 
+  filter(Before > 0) %>% 
+  mutate(Change = Before-After) %>%
+  mutate(OpinChange = case_when(
+    Change > 0 ~ 1, # Positive change
+    Change < 0 ~ 3, # Negative change
+    Change == 0 ~ 2)) %>% # No change
+  drop_na(OpinChange) %>%
+  mutate(TotTrps = as.numeric(YrsVol) * as.numeric(Tpy)) 
+
+  lr <- mydata %>% # format variables for logistic regression
+  mutate(OpinChange = as.factor(OpinChange)) %>% 
+  mutate(NumYrStart = as.numeric(YrStart)) %>%
+  mutate(NumEvnts = as.numeric(Evnts)) %>% 
+  mutate(NumEvnts = replace_na(NumEvnts, 0)) %>% # NA = 0 volunteer events attended
+  filter(NumYrStart > 0) %>%
+  # drop_na(TotTrps) %>% 
+  mutate(YrsSince = 2018 - NumYrStart) %>% 
+  dplyr::select(OpinChange,YrsSince,TotTrps,NumEvnts)
+
+# run full multinomial logistic regression model (all variables)
+# default reference category is 1 if y is based on numeric factors
+# therefore, reference category is Positive Change (in opinion)
+fullmod <- multinom(OpinChange~YrsSince+TotTrps+NumEvnts, 
+               data = lr,
+               Hess = TRUE,
+               na.action = na.exclude) 
+
+# test model fit
+fullGFT <- Anova(fullmod, type="II") # Anova table of likelihood ratio tests
+fullGFT
+
+# individual effects
+tdytbl <- tidy(fullmod, conf.int = TRUE)
+tdytbl
+
+# predicted vals
+# https://cran.r-project.org/web/packages/MNLpred/vignettes/OVA_Predictions_For_MNL.html
+pred1 <- mnl_pred_ova(model = fullmod,
+                      data = lr,
+                      xvari = "YrsSince",
+                      by = 0.25,
+                      seed = "525625", 
+                      nsim = 100, # faster than the default 1000
+                      probs = c(0.025, 0.975)) # default
+
+# label opinion change categories
+library(scales)
+OC_cat <- c(
+  "1" = "Positive Change",
+  "2" = "No Change",
+  "3" = "Negative Change")
+
+# determine how long before pred. prob. of pos. opin. change >= 50%
+y.50 <- which(pred1$plotdata[1:73,3] >= 0.50) # y vals >= 50%
+x.50 <- first(y.50) # index of x val where first y val >= 50%
+x.val <- pred1$plotdata[x.50,1] # coressponding x val
+
+# plot predicted probabilites across opin. change categories
+ggplot(data = pred1$plotdata, aes(x = YrsSince, y = mean,
+                                  ymin = lower, ymax = upper)) +
+  geom_ribbon(alpha = 0.1) + # Confidence intervals
+  geom_line() + # Mean
+  facet_wrap(OpinChange ~.,
+             labeller = labeller(OpinChange = OC_cat)) +
+  geom_segment(aes(x = 7.25, y = .5, xend = 0, yend = .5), linetype = "dashed",data = pred1$plotdata[1:73,])+
+  geom_segment(aes(x = 7.25, y = .5, xend = 7.25, yend = 0), linetype = "dashed",data = pred1$plotdata[1:73,])+
+  
+  scale_y_continuous(labels = percent_format(accuracy = 1)) + # % labels
+  theme_bw() +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=12),
+        strip.text = element_text(size=12),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank(),
+        panel.border = element_rect(colour = "black"))+
+  labs(y = "Predicted probability",
+       x = "Years since joining CCFRP")
+
+
